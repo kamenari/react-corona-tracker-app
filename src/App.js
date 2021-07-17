0ad6d21d1464a965fb93cdf1ca19c0f153414dfc
takahashikazunari <kamenaris@gmail.com>
2021-07-17 13:03:26 +0900

    apiから取得した各データをcountryDataで定義した各値にset、新規感染者と新規回復者の計算

--- src/App.js
+++ src/App.js
@@ -5,13 +5,29 @@ import './App.css';
 
 function App() {
   const [country, setCountry] = useState("");
-    const getCountryData = () => {
-        fetch(`https://api.covid19api.com/country/${country}`)
-        .then(res => res.json())
-        .then(data => console.log(data))
-    }
+  const [countryData, setCountryData] =useState({
+    data: "",
+    newConfirmed: "",
+    totalConfirmed: "",
+    newRecovered: "",
+    totalRecovered: "",
+  });
+  const getCountryData = () => {
+      fetch(`https://api.covid19api.com/country/${country}`)
+      .then(res => res.json())
+      .then(data => {
+        setCountryData({
+          data: data[data.length -1].Data,
+          newConfirmed: data[data.length -1].Confirmed - data[data.length -2].Confirmed,
+          totalConfirmed: data[data.length -1].Confirmed,
+          newRecovered: data[data.length -1].Recovered - data[data.length -2].Recovered,
+          totalRecovered: data[data.length -1].Recovered,
+        });
+      })
+  }
   return (
     <div>
+      {console.log(countryData)}
       <TopPage countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData} />
     </div>
   );
