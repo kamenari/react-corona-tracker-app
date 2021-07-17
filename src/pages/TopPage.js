f13872799550365fc875a7f7a8d045803e4721de
takahashikazunari <kamenaris@gmail.com>
2021-07-17 12:32:44 +0900

    Selectorのロジック部分をappに移す、Resultsコンポーネント作成、appからtoppage、toppageからselecterにpropsでデータを渡す

--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -2,11 +2,11 @@ import Title from "../components/Title";
 import Selector from "../components/Selector";
 import Results from "../components/Results";
 
-const TopPage = () => {
+const TopPage = (props) => {
     return (
         <div>
             <Title />
-            <Selector />
+            <Selector countriesJson={props.countriesJson} setCountry={props.setCountry} getCountryData={props.getCountryData} />
             <Results />
         </div>
     );
