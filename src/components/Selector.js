dee2c87688eee0d85e126926cbafa6303250eeb5
takahashikazunari <kamenaris@gmail.com>
2021-07-20 22:04:36 +0900

    セレクトを修正

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -2,6 +2,7 @@ const Selector = ({ setCountry, countriesJson }) => {
     return (
         <div className="selector-container">
             <select onChange={(e) => setCountry(e.target.value)}>
+                <option>Select A Country</option>
                 {countriesJson.map((country, index) =>
                     <option key={index} value={country.Slug}>{country.Country}</option>
                 )}
