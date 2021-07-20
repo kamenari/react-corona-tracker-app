14e68563837fd886ad82000d2f2c5500c95c908b
takahashikazunari <kamenaris@gmail.com>
2021-07-20 22:09:25 +0900

    国名を表示するように改修

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,13 +1,14 @@
 import Loading from "./Loading"; 
 
 const Results = ({ countryData, loading }) => {
-    const { data, newConfirmed, totalConfirmed, newRecovered, totalRecovered } = countryData;
+    const { data, newConfirmed, totalConfirmed, newRecovered, totalRecovered, countryName } = countryData;
     return (
         <div className="results-container">
             {loading ? 
                 <Loading />
                 : 
                 <div>
+                    <p>国名: <span>{countryName}</span></p>
                     <p>日付: <span>{data.slice(0, 10)}</span></p>
                     <p>新規感染者: <span>{newConfirmed.toLocaleString()}</span></p>
                     <p>感染者総数: <span>{totalConfirmed.toLocaleString()}</span></p>
